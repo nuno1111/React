51c89ef7e8347fa88891bff39f9d4a4bf70de147
한동훈 <nuno1026@gmail.com>
2020-10-18 21:54:07 +0900

    react study 2020-10-18

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from "react";
-// import MyComponent from "./MyComponent";
-// import Counter from "./Counter";
-// import Say from "./Say";
-import EventPractice from "./EventPractice";
+import React, { Component } from "react";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
 
-const App = () => {
-  return (
-    // <MyComponent name="React" favoriteNumber={1}>
-    //   React Children
-    // </MyComponent>
-    <EventPractice />
-  );
-};
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/IterationSapmle.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const IterationSapmle = () => {
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ]);
+
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
+
+  const onChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const onClick = () => {
+    console.log(nextId, inputText);
+    setNames([...names, { id: nextId, text: inputText }]);
+    setNextId(nextId + 1);
+    setInputText("");
+  };
+
+  const onRemove = (id) => {
+    setNames(names.filter((name) => name.id !== id));
+  };
+
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+    </>
+  );
+};
+export default IterationSapmle;
